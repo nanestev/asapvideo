6fb36332714ad886ca122724ce2e27684dc2955f
Nikolay Anestev <nikolay@styloko.com>
2016-06-08 15:52:28 +0100

    Arbitrary image count. Audio background looping.

---
+++ asapvideo/asapvideo.py
@@ -0,0 +1,58 @@
+import os
+import imghdr
+from ffmpy import FFmpeg
+from collections import OrderedDict
+import random
+import math
+
+fps = 25
+scene_duration_t = 5
+scene_duration_f = scene_duration_t * fps
+transition_t = 0.5
+effects = ["zoompan=z='min(zoom+0.0015,1.5)':d={df}", "zoompan=z='if(lte(zoom,1.0),1.5,max(1.001,zoom-0.0015))':d={df}"]
+scene_filter = "[{n}:v]{effect},trim=duration={dt},fade=t=in:st=0:d={tt},fade=t=out:st={te}:d={tt},scale=1296x866,setpts=PTS-STARTPTS[v{n}]"
+audio_fade_out_t = 4
+audio_tracks = [("track1.wav", 11), ("track2.wav", 11), ("track3.wav", 13)]
+
+def make_from_dir(dir):
+    # add all image files in the folder as input
+    inputs = OrderedDict([(f, None) for f in os.listdir(dir) if imghdr.what(f) != None])
+
+    # exit if no images were found
+    if bool(inputs) == False:
+        return False
+
+    count = len(inputs)
+    # calculate the length of the whole video
+    lenght_t = scene_duration_t*count
+    # select audio track
+    audio_track = audio_tracks[random.randint(0, len(audio_tracks)-1)] 
+    # calculate number of loops for the audio track
+    audio_track_repetition = int(math.ceil(lenght_t / float(audio_track[1])))
+
+    # create all video streams by applying effects to every image we found
+    applied_filters = [scene_filter.format(n=ind, effect=effects[ind % 2].format(df=scene_duration_f), dt=scene_duration_t, tt=transition_t, te=scene_duration_t-transition_t) for ind, x in enumerate(inputs)]
+    # copy the audio track into the required number of audio streams
+    applied_filters.append(";".join(["[{n}:a]afifo[a{i}]".format(n=count, i=i) for i in range(audio_track_repetition)]))
+    # concatenate the audio streams into a single audio loop stream
+    applied_filters.append("".join(["[a{i}]".format(i=i) for i in range(audio_track_repetition)]) + "concat=n={n}:v=0:a=1[a]".format(n=audio_track_repetition))
+    # trim the audio stream to the required number of seconds and applies fade out effect
+    applied_filters.append("[a]aselect=between(t\,0\,{d}),volume='if(lt(t,{e}),1,max(1-(t-{e})/{ae},0))':eval=frame[audio]".format(d=lenght_t, e=lenght_t-audio_fade_out_t, ae=audio_fade_out_t))
+    # concatenate all video streams into a single stream
+    applied_filters.append("{tags} concat=n={count}:v=1:a=0 [video]".format(tags="".join(["[v{0}]".format(ind) for ind, x in enumerate(inputs)]), count=count))
+
+    # add the audio track to the inputs collection
+    inputs.update({audio_track[0]: None})
+
+    # build the video
+    ff = FFmpeg(
+        global_options = ["-y"],
+        inputs = inputs,
+        outputs = {"out.mp4": "-filter_complex \"" + ";".join(applied_filters) + "\" -map \"[video]\" -map \"[audio]\" -c:v libx264 -pix_fmt yuvj420p -q:v 1"}
+	)
+    #print ff.cmd
+    ff.run()
+    return True
+    
+if __name__ == "__main__":
+    make_from_dir(".")
