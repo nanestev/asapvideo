7403f1b7b5089167ba27531f981f59f7099a18b7
Nikolay Anestev <nikolay@styloko.com>
2016-06-10 10:46:52 +0100

    define module constants. define function that will work with url list.

--- asapvideo/asapvideo.py
+++ asapvideo/asapvideo.py
@@ -8,35 +8,60 @@ import math
 import urllib2
 import json
 import string
+import re
 
-fps = 25
-scene_duration_t = 5
-scene_duration_f = scene_duration_t * fps
-transition_t = 0.5
-width = 1000
-height = 1000
-effects = ["zoompan=z='min(zoom+0.0015,1.5)':d={df}", "zoompan=z='if(lte(zoom,1.0),1.5,max(1.001,zoom-0.0015))':d={df}"]
-scene_filter = "[{n}:v]{effect},trim=duration={dt},fade=t=in:st=0:d={tt},fade=t=out:st={te}:d={tt},scale='iw*min({w}/iw\,{h}/ih)':'ih*min({w}/iw\,{h}/ih)', pad={w}:{h}:'({w}-iw*min({w}/iw\,{h}/ih))/2':'({h}-ih*min({w}/iw\,{h}/ih))/2',setpts=PTS-STARTPTS[v{n}]"
-audio_fade_out_t = 4
-audio_tracks_index_url = "https://s3.amazonaws.com/asapvideo/audio/tracks.json"
+FPS = 25
+SCENE_DURATION_T = 5
+SCENE_DURATION_F = SCENE_DURATION_T * FPS
+TRANSITION_T = 0.5
+OUTPUT_VIDEO_WIDTH = 1200
+OUTPUT_VIDEO_HEIGHT = 800
+AUDIO_FADE_OUT_T = 4
+AUDIO_TRACKS_INDEX_URL = "https://s3.amazonaws.com/asapvideo/audio/tracks.json"
 
-def make_from_dir(dir):
+def make_from_dir(dir,outdir=dir):
     # add all image files in the folder as input
-    inputs = OrderedDict([(ff, None) for ff in [os.path.join(dir,f) for f in os.listdir(dir)] if imghdr.what(ff) != None])
+    return _make(OrderedDict([(ff, None) for ff in [os.path.join(dir,f) for f in os.listdir(dir)] if imghdr.what(ff) != None]), outdir)
 
+def make_from_url_list(list,outdir=None):
+    regex = r'('
+    # Scheme (HTTP, HTTPS, FTP and SFTP):
+    regex += r'(?:(https?|s?ftp):\/\/)?'
+    # www:
+    regex += r'(?:www\.)?'
+    regex += r'('
+    # Host and domain (including ccSLD):
+    regex += r'(?:(?:[A-Z0-9][A-Z0-9-]{0,61}[A-Z0-9]\.)+)'
+    # TLD:
+    regex += r'([A-Z]{2,6})'
+    # IP Address:
+    regex += r'|(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+    regex += r')'
+    # Port:
+    regex += r'(?::(\d{1,5}))?'
+    # Query path:
+    regex += r'(?:(\/\S+)*)'
+    regex += r')'
+    prog = re.compile(regex, re.IGNORECASE)
+    # add all urls that are recognised as correct, return status code 200 and image content type
+    return _make(OrderedDict([(r.geturl(), None) for r in [urllib2.urlopen(u) for u in list if prog.match(u)] if r.getcode() == 200 and r.info().getheader('Content-Type').startswith("image")]), outdir)
+
+def _make(inputs, dir):
     # exit if no images were found
     if bool(inputs) == False:
-        return False
+        return None
 
     count = len(inputs)
     # calculate the length of the whole video
-    lenght_t = scene_duration_t*count
+    lenght_t = SCENE_DURATION_T*count
+    effects = ["zoompan=z='min(zoom+0.0015,1.5)':d={df}", "zoompan=z='if(lte(zoom,1.0),1.5,max(1.001,zoom-0.0015))':d={df}"]
+    scene_filter = "[{n}:v]{effect},trim=duration={dt},fade=t=in:st=0:d={tt},fade=t=out:st={te}:d={tt},scale='iw*min({w}/iw\,{h}/ih)':'ih*min({w}/iw\,{h}/ih)', pad={w}:{h}:'({w}-iw*min({w}/iw\,{h}/ih))/2':'({h}-ih*min({w}/iw\,{h}/ih))/2',setpts=PTS-STARTPTS[v{n}]"
 
     # load available audio tracks info
     audio_track = None
     try:
         # loads the index from url
-        response = urllib2.urlopen(audio_tracks_index_url)
+        response = urllib2.urlopen(AUDIO_TRACKS_INDEX_URL)
         content = response.read()
         audio_tracks_index = json.loads(content.translate(None, string.whitespace))
         # extracts all suitable tracks
@@ -49,7 +74,7 @@ def make_from_dir(dir):
         pass
 
     # create all video streams by applying effects to every image we found
-    applied_filters = [scene_filter.format(n=ind, effect=effects[ind % 2].format(df=scene_duration_f), dt=scene_duration_t, tt=transition_t, te=scene_duration_t-transition_t, w=width, h=height) for ind, x in enumerate(inputs)]
+    applied_filters = [scene_filter.format(n=ind, effect=effects[ind % 2].format(df=SCENE_DURATION_F), dt=SCENE_DURATION_T, tt=TRANSITION_T, te=SCENE_DURATION_T-TRANSITION_T, w=OUTPUT_VIDEO_WIDTH, h=OUTPUT_VIDEO_HEIGHT) for ind, x in enumerate(inputs)]
     # concatenate all video streams into a single stream
     applied_filters.append("{tags} concat=n={count}:v=1:a=0 [video]".format(tags="".join(["[v{0}]".format(ind) for ind, x in enumerate(inputs)]), count=count))
 
@@ -61,19 +86,18 @@ def make_from_dir(dir):
         # concatenate the audio streams into a single audio loop stream
         applied_filters.append("".join(["[a{i}]".format(i=i) for i in range(audio_track_repetition)]) + "concat=n={n}:v=0:a=1[a]".format(n=audio_track_repetition))
         # trim the audio stream to the required number of seconds and applies fade out effect
-        applied_filters.append("[a]aselect=between(t\,0\,{d}),volume='if(lt(t,{e}),1,max(1-(t-{e})/{ae},0))':eval=frame[audio]".format(d=lenght_t, e=lenght_t-audio_fade_out_t, ae=audio_fade_out_t))
+        applied_filters.append("[a]aselect=between(t\,0\,{d}),volume='if(lt(t,{e}),1,max(1-(t-{e})/{ae},0))':eval=frame[audio]".format(d=lenght_t, e=lenght_t-AUDIO_FADE_OUT_T, ae=AUDIO_FADE_OUT_T))
         # add the audio track to the inputs collection
         inputs.update({audio_track[0]: None})
 
     # build the video
+    output = "video.mp4"
+    output = os.path.join(dir,output) if dir else output
     ff = FFmpeg(
         global_options = ["-y"],
         inputs = inputs,
-        outputs = {os.path.join(dir,"out.mp4"): "-filter_complex \"" + ";".join(applied_filters) + "\" -map \"[video]\"" + (" -map \"[audio]\"" if audio_track else "") + " -c:v libx264 -pix_fmt yuvj420p -q:v 1"}
+        outputs = {output: "-filter_complex \"" + ";".join(applied_filters) + "\" -map \"[video]\"" + (" -map \"[audio]\"" if audio_track else "") + " -c:v libx264 -pix_fmt yuvj420p -q:v 1"}
 	)
     #print ff.cmd
     ff.run()
-    return True
-    
-if __name__ == "__main__":
-    make_from_dir(".")
+    return output
